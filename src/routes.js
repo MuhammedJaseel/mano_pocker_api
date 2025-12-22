81dfd3030aa2f713b8f754a7bafba061660157e2
MuhammedJaseel <jaseelmanamulli@gmail.com>
2025-12-22 16:06:31 +0400

    OP

--- src/routes.js
+++ src/routes.js
@@ -1,58 +1,8 @@
 import express from "express";
-import mongoose from "mongoose";
+
 const router = express.Router();
 
-router.get("/api/accounts", async (req, res) => {
-  const collection = mongoose.connection.db.collection("wallets");
-  const data = await collection
-    .find({}, { projection: { _id: 0, a: 1, b: 1, n: 1 } })
-    .skip(0)
-    .limit(100)
-    .sort({ _id: -1 })
-    .toArray();
-  const total = await collection.countDocuments();
-  return res.json({ data, total });
-});
-
-router.get("/api/transactions", async (req, res) => {
-  const collection = mongoose.connection.db.collection("txns");
-  const data = await collection
-    .find({}, { projection: { _id: 0, th: 1, v: 1, f: 1, t: 1, gu: 1, ca: 1 } })
-    .skip(0)
-    .limit(100)
-    .sort({ _id: -1 })
-    .toArray();
-  const total = await collection.countDocuments();
-  return res.json({ data, total });
-});
-
-router.get("/api/blocks", async (req, res) => {
-  const collection = mongoose.connection.db.collection("blocks");
-  const data = await collection
-    .find(
-      {},
-      {
-        projection: {
-          _id: 0,
-          bh: 1,
-          ph: 1,
-          bn: 1,
-          ph: 1,
-          tsx: 1,
-          ts: 1,
-          ca: 1,
-        },
-      }
-    )
-    .skip(0)
-    .limit(100)
-    .sort({ _id: -1 })
-    .toArray();
-  const total = await collection.countDocuments();
-  return res.json({ data, total });
-});
-
-router.post("/pocker-api/create-room", (req, res) => {
+router.post("/api/room", (req, res) => {
   // "number_of_players": 4,
   // "names": ["Alice", "Bob", "Chloe", "Dan"],
   // "max_amount": 200,
@@ -72,11 +22,25 @@ router.post("/pocker-api/create-room", (req, res) => {
     });
   }
 
+  const roomId = new Date().getTime().toString(36);
+
   const result = {
-    room_id: "12345",
+    room_id: roomId,
     acccounts,
   };
   return res.json(result);
+});
+
+router.get("/api/room", (req, res) => {
+  return res.json({});
+});
+
+router.post("/api/join", (req, res) => {
+  // "roomId": "1234",
+  // "userId": 0,
+  // "userAddress":"Ox0000000000000000000000000000000000000000"
+
+  res.json({});
 });
 
 export default router;
